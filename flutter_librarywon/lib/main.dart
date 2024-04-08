bb72c33ef059fedb833d1099c81d615deb58897c
librarywon <librarywon@naver.com>
2024-04-08 15:43:42 +0900

    feat: add HomeworkWidget

--- flutter_librarywon/lib/main.dart
+++ flutter_librarywon/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'home/homework_widget.dart';
+import 'home/title_text_widget.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -15,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: "Week2"),
+      home: const MyHomePage(title: "GDSC 모바일 스터디"),
     );
   }
 }
@@ -29,75 +32,52 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(title),
+        title: TitleTextWidget(text: title),
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(1.0),
+          child: Divider(
+            height: 1.0,
+            color: Color(0xFFDCDCDC),
+            thickness: 1.0,
+          ),
+        ),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CustomColorContainer(
-                  color: Colors.red, marginRight: 5, marginBottom: 5),
-              CustomColorContainer(
-                  color: Colors.blue, marginLeft: 5, marginBottom: 5),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CustomColorContainer(
-                  color: Colors.yellow, marginRight: 5, marginTop: 5),
-              CustomColorContainer(
-                  color: Colors.green, marginLeft: 5, marginTop: 5),
-            ],
-          ),
-          Container(
-            margin: const EdgeInsets.only(top: 20), 
-            child: Text(
-              "librarywon Flutter",
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 18,
-              ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            TitleTextWidget(text: "과제"),
+            SizedBox(height: 20.0),
+            Row(
+              mainAxisAlignment:
+                  MainAxisAlignment.spaceBetween, // 자식 위젯들 사이에 공간을 균등하게 분배
+              children: [
+                Expanded(
+                  // 첫 번째 HomeworkWidget을 Expanded로 감싸기
+                  child: HomeworkWidget(
+                    text: '오늘의 과제',
+                    imagePath: 'assets/images/ic_home_today_homework.svg',
+                  ),
+                ),
+                SizedBox(width: 40.0),
+                Expanded(
+                  // 두 번째 HomeworkWidget을 Expanded로 감싸기
+                  child: HomeworkWidget(
+                    text: '수행한 과제',
+                    imagePath: 'assets/images/ic_home_done_homework.svg',
+                  ),
+                ),
+              ],
             ),
-          ),
-        ],
+            SizedBox(height: 40.0),
+            TitleTextWidget(text: "나의 수행도"),
+            SizedBox(height: 40.0),
+            
+          ],
+        ),
       ),
     );
-  }
-}
-
-
-class CustomColorContainer extends StatelessWidget {
-  final Color color;
-  final double marginLeft;
-  final double marginTop;
-  final double marginRight;
-  final double marginBottom;
-
-  const CustomColorContainer({
-    Key? key,
-    required this.color,
-    this.marginLeft = 0,
-    this.marginTop = 0,
-    this.marginRight = 0,
-    this.marginBottom = 0,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        width: 100,
-        height: 100,
-        margin: EdgeInsets.fromLTRB(
-            marginLeft, marginTop, marginRight, marginBottom),
-        decoration: BoxDecoration(
-          color: color,
-          borderRadius: BorderRadius.all(
-            Radius.circular(5),
-          ),
-        ));
   }
 }
