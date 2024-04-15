2f3f3d03e5ee54b926fb6c65157cef0af3a5f5a4
librarywon <librarywon@naver.com>
2024-04-15 15:44:02 +0900

    build: remove week3

--- flutter_librarywon/lib/main.dart
+++ flutter_librarywon/lib/main.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'home/custom_calendar_widget.dart';
-import 'home/homework_widget.dart';
-import 'home/title_text_widget.dart';
-
 void main() {
   runApp(const MyApp());
 }
@@ -33,7 +29,7 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: TitleTextWidget(text: title),
+          title: Text(title),
           bottom: PreferredSize(
             preferredSize: Size.fromHeight(1.0),
             child: Divider(
@@ -43,53 +39,8 @@ class MyHomePage extends StatelessWidget {
             ),
           ),
         ),
-        body: SingleChildScrollView(
-          // 스크롤 가능하게 만들기 위해 SingleChildScrollView 추가
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                TitleTextWidget(text: "과제"),
-                SizedBox(height: 20.0),
-                Row(
-                  mainAxisAlignment:
-                      MainAxisAlignment.spaceBetween, // 자식 위젯들 사이에 공간을 균등하게 분배
-                  children: [
-                    Expanded(
-                      // 첫 번째 HomeworkWidget을 Expanded로 감싸기
-                      child: HomeworkWidget(
-                        text: '오늘의 과제',
-                        imagePath: 'assets/images/ic_home_today_homework.svg',
-                      ),
-                    ),
-                    SizedBox(width: 40.0),
-                    Expanded(
-                      // 두 번째 HomeworkWidget을 Expanded로 감싸기
-                      child: HomeworkWidget(
-                        text: '수행한 과제',
-                        imagePath: 'assets/images/ic_home_done_homework.svg',
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 40.0),
-                TitleTextWidget(text: "나의 수행도"),
-                SizedBox(height: 20.0),
-                CustomCalendarWidget(),
-                SizedBox(height: 20.0),
-                TitleTextWidget(text: "나의 다짐"),
-                SizedBox(height: 20.0),
-                Text("네이티브 화이팅",
-                    style: TextStyle(
-                      fontFamily: 'Pretendard',
-                      fontWeight: FontWeight.w500,
-                      fontSize: 16,
-                    ))
-              ],
-            ),
-          ),
+        body: Column(
+          children: [],
         ));
   }
 }
